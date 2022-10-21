83aa94a70cadb84d53e246a426d41b1ec9a275ac
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2022-10-21 19:15:34 +0700

    add health check

--- src/main/kotlin/com/example/sonnh/controllers/UploadFileController.kt
+++ src/main/kotlin/com/example/sonnh/controllers/UploadFileController.kt
@@ -4,6 +4,7 @@ import com.example.sonnh.services.CsvService
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/upload")
 class UploadFileController {
 
@@ -18,12 +20,15 @@ class UploadFileController {
     private lateinit var csvService: CsvService
 
     @PostMapping("/csv")
-    fun uploadCsv(
-        @RequestParam("file") multipartfile: MultipartFile,
-        @RequestParam("keys") keysStr: String
-    ): String {
+    fun uploadCsv(@RequestParam("file") multipartfile: MultipartFile, @RequestParam("keys") keysStr: String): String {
         val keys: Collection<String> = jacksonObjectMapper().readValue(keysStr)
         csvService.uploadCsvFile(multipartfile, keys)
         return "kotlin"
     }
+
+    @PostMapping("/ping")
+    fun ping(): String {
+        return "bong"
+    }
+
 }
